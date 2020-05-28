6c7842c57e5b06e0a2046e29273d2b0735cf5791
Goutam P Singh <gautam_prakash_singh@yahoo.com>
2020-05-28 11:57:41 +0530

    first commit

---
+++ redux-counter/src/Counter.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+const containerStyle = {
+  display: "flex",
+};
+const buttonStyle = {
+  fontSize: "1.5rem",
+  width: "40px",
+  height: "40px",
+};
+
+class Counter extends Component {
+  //   constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //       number: 0,
+  //     };
+  //   }
+
+  //   addOne = () => {
+  //     this.setState({
+  //       number: this.state.number + 1,
+  //     });
+  //   };
+
+  //   minusOne = () => {
+  //     this.setState({
+  //       number: this.state.number - 1,
+  //     });
+  //   };
+
+  addOne = () => {
+    this.props.dispatch({ type: "ADD_ONE" });
+  };
+
+  minusOne = () => {
+    this.props.dispatch({ type: "MINUS_ONE" });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>{this.props.number}</h1>
+          <div style={containerStyle}>
+            <button type="button" style={buttonStyle} onClick={this.minusOne}>
+              -
+            </button>
+            <button type="button" style={buttonStyle} onClick={this.addOne}>
+              +
+            </button>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    number: state.number,
+  };
+};
+
+export default connect(mapStateToProps)(Counter);
